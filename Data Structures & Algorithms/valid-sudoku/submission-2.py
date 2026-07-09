df96be69538fab7f89d906d4c136637f4ad99796
Naveen699 <63660800+Naveen699@users.noreply.github.com>
2026-07-09 13:21:14 +1000

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        seen = set()
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                value = board[i][j]
+
+                if value == ".":
+                    continue
+
+                row = (value, "row", i)
+                column = (value, "column", j)
+                sub_box = (value, "sub_box", i // 3, j // 3)
+
+                if row in seen or column in seen or sub_box in seen:
+                    return False
+                
+                seen.add(row)
+                seen.add(column)
+                seen.add(sub_box)
+
+        return True
